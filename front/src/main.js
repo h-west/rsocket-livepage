b0208c04a5f71b18a3174d33bf34a47735b41d5f
hsjang <hsjang@hsjangui-MacBookPro.local>
2020-01-27 00:35:22 +0900

    add

--- front/src/main.js
+++ front/src/main.js
@@ -6,7 +6,7 @@ import store from './plugins/store';
 import vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false
-Vue.use(rsclient,{url:'ws://192.168.0.3:8080/ws'})
+Vue.use(rsclient,{url:'ws://52.78.139.246:8080/ws'})
 
 new Vue({
   router,
